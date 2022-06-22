d77fef772e38c21cfe9af03c04d527e157e4d6a6
Robosalty <92951449+Robosalty@users.noreply.github.com>
2022-06-22 14:50:22 -0700

    fixed infinite scroll AGAIN

--- HOME/home.js
+++ HOME/home.js
@@ -9,11 +9,11 @@ window.addEventListener('scroll', movement);
 function movement(){    
     const value = window.scrollY;
 
-    stars.style.left = Math.min(value * 0.25, stars) + 'px';
-    moon.style.top = Math.min(value * 1.05, moon) + 'px';
-    mountains_behind.style.top = Math.min(value * 0.5, mountains_behind) + 'px';
+    stars.style.left = Math.min(value * 0.25, stars.height) + 'px';
+    moon.style.top = Math.min(value * 1.05, moon.height/2) + 'px';
+    mountains_behind.style.top = Math.min(value * 0.5, mountains_behind.height/2) + 'px';
     btn.style.marginTop = Math.min(value * 1.5) + 'px';
-    mountains_front.style.top = Math.min(value * 0, mountains_front) + 'px';
+    mountains_front.style.top = Math.min(value * 0, mountains_front.height/2) + 'px';
 
 }    
 
