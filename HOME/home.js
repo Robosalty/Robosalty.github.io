571ed9a19aad266e140254a835bcee5771dd8727
Robosalty <92951449+Robosalty@users.noreply.github.com>
2022-06-22 14:47:15 -0700

    fixed infinite scroll

--- HOME/home.js
+++ HOME/home.js
@@ -7,10 +7,13 @@ let mountains_front = document.getElementById('mountains_front');
 window.addEventListener('scroll', movement);
 
 function movement(){    
-    let value = window.scrollY;
-    stars.style.left = value * 0.25 + 'px';
-    moon.style.top = value * 1.05 + 'px';
-    mountains_behind.style.top = value * 0.5 + 'px';
-    btn.style.marginTop = value * 1.5 + 'px';
-    mountains_front.style.top = value * 0 + 'px';
+    const value = window.scrollY;
+
+    stars.style.left = Math.min(value * 0.25, stars) + 'px';
+    moon.style.top = Math.min(value * 1.05, moon) + 'px';
+    mountains_behind.style.top = Math.min(value * 0.5, mountains_behind) + 'px';
+    btn.style.marginTop = Math.min(value * 1.5) + 'px';
+    mountains_front.style.top = Math.min(value * 0, mountains_front) + 'px';
+
 }    
+
