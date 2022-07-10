925950f0b7c04dd41d79fa949edeb6d7739af80b
Robosalty <92951449+Robosalty@users.noreply.github.com>
2022-07-10 15:54:32 -0700

    idk

--- ABOUT/about.js
+++ ABOUT/about.js
@@ -14,3 +14,28 @@ for (i = 0; i < len; i++){
         }
     })
 }
+
+
+/**********************************************************************************************/
+/*                                 SCROLL TO TOP BUTTON                                       */
+/**********************************************************************************************/
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
